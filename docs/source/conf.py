c1be4b814fbbe6fb81d90a7a65c69e85cbefa318
Aadit Tambe <aadit.tambe@gmail.com>
2022-02-28 20:25:33 +0000

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+source_suffix = '.md'
+
+root_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
